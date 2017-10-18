dc5a79327695d8a63363c823f123717894168d2f
3424 <3424@example.com>
2017-10-18 10:18:17 +0900

    画像表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :icons
+  resources :icons do
+    member do
+      get'show_image'
+    end 
+  end
   resources :sches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
