c31cd6169d710c4d3cc75c54cfa7e26369456d18
3424 <3424@example.com>
2018-01-11 11:09:35 +0900

    ジャンルカラー

--- test/controllers/syojis_controller_test.rb
+++ test/controllers/syojis_controller_test.rb
@@ -17,7 +17,7 @@ class SyojisControllerTest < ActionDispatch::IntegrationTest
 
   test "should create syoji" do
     assert_difference('Syoji.count') do
-      post syojis_url, params: { syoji: { artist: @syoji.artist, category: @syoji.category, color_j: @syoji.color_j, image_binary: @syoji.image_binary, image_syo: @syoji.image_syo, image_type: @syoji.image_type, mail_syo: @syoji.mail_syo, memo_syo: @syoji.memo_syo, notification_syo: @syoji.notification_syo, notification_time: @syoji.notification_time, release: @syoji.release, snooze: @syoji.snooze, title_j: @syoji.title_j, title_syo: @syoji.title_syo, url_syo: @syoji.url_syo } }
+      post syojis_url, params: { syoji: { artist: @syoji.artist, category: @syoji.category, color_id: @syoji.color_id, image_binary: @syoji.image_binary, image_syo: @syoji.image_syo, image_type: @syoji.image_type, mail_syo: @syoji.mail_syo, memo_syo: @syoji.memo_syo, notification_syo: @syoji.notification_syo, notification_time: @syoji.notification_time, release: @syoji.release, snooze: @syoji.snooze, title_j: @syoji.title_j, title_syo: @syoji.title_syo, url_syo: @syoji.url_syo } }
     end
 
     assert_redirected_to syoji_url(Syoji.last)
@@ -34,7 +34,7 @@ class SyojisControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update syoji" do
-    patch syoji_url(@syoji), params: { syoji: { artist: @syoji.artist, category: @syoji.category, color_j: @syoji.color_j, image_binary: @syoji.image_binary, image_syo: @syoji.image_syo, image_type: @syoji.image_type, mail_syo: @syoji.mail_syo, memo_syo: @syoji.memo_syo, notification_syo: @syoji.notification_syo, notification_time: @syoji.notification_time, release: @syoji.release, snooze: @syoji.snooze, title_j: @syoji.title_j, title_syo: @syoji.title_syo, url_syo: @syoji.url_syo } }
+    patch syoji_url(@syoji), params: { syoji: { artist: @syoji.artist, category: @syoji.category, color_id: @syoji.color_id, image_binary: @syoji.image_binary, image_syo: @syoji.image_syo, image_type: @syoji.image_type, mail_syo: @syoji.mail_syo, memo_syo: @syoji.memo_syo, notification_syo: @syoji.notification_syo, notification_time: @syoji.notification_time, release: @syoji.release, snooze: @syoji.snooze, title_j: @syoji.title_j, title_syo: @syoji.title_syo, url_syo: @syoji.url_syo } }
     assert_redirected_to syoji_url(@syoji)
   end
 
