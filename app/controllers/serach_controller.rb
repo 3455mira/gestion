b50072ca7cf289836f85d53b7349ac8241114586
3429 <3429@example.com>
2018-01-18 11:07:38 +0900

    検索（一部一致）

--- app/controllers/serach_controller.rb
+++ app/controllers/serach_controller.rb
@@ -5,5 +5,6 @@ class SerachController < ApplicationController
   def search
     name=params[:name]
     @sche = Sche.where(user_id: session[:usr].to_i).where("name_sche like '%"+name+"%'")
+    @syoji = Syoji.where(user_id: session[:usr].to_i).where("title_syo like '%"+name+"%'")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where(id: session[:usr])
   end
 
   # GET /users/1
