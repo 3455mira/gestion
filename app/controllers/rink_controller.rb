7c65227b832ac67e41e347cecc4f37ae8c56c890
3429 <3429@example.com>
2018-01-29 10:36:21 +0900

    リンク一覧

--- app/controllers/rink_controller.rb
+++ app/controllers/rink_controller.rb
@@ -1,5 +1,6 @@
 class RinkController < ApplicationController
   def index
-    @rinks = Syoji.where(user_id: session[:usr]).select(:url_syo,:title_syo).distinct.order("url_syo")
+    @links1 = Syoji.where(user_id: session[:usr]).select(:url_syo,:title_syo).distinct.order("url_syo")
+    @links2 = Sche.where(user_id: session[:usr]).select(:url_sche,:name_sche).distinct.order("url_sche")
   end
 end
